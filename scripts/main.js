a075a27b524039337765f45c1c3374e3fdb81443
ThaliaCS <thalia.schone@gmail.com>
2021-05-23 19:24:28 -0300

    feat: created workout table

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,83 @@
-const createLog = (event) => {
-  event.preventDefault();
-  const array = [];
+import DeleteButton from "./deleteWorkout.js";
 
-  const teste = document.querySelector("[data-teste]");
-  const time = document.querySelector("[data-time-input]");
-  array.push(time.value);
+const fields = [
+  {
+    id: "time-input",
+    name: "time",
+  },
+  {
+    id: "workout-input",
+    name: "workout",
+  },
+  {
+    id: "date-input",
+    name: "date",
+  },
+  {
+    id: "action",
+    name: "action",
+  },
+];
 
-  const workout = document.querySelector("[data-workout-input]");
-  array.push(workout.value);
+let totalTime = 0;
 
-  const date = document.querySelector("[data-date]");
-  array.push(date.value);
+const getWorkoutData = () => {
+  const data = {};
+  fields.forEach((field) => {
+    const element = document.querySelector(`#${field.id}`);
+    if(element !== null){
+   
+      data[field.name] = element.value;
+    }
+  });
 
-  const table = document.createElement("p");
-
-  //Somete printando em um p para visualizar
-  const conteudo = `<p>${array}</p>`;
-  console.log(array);
-
-  table.innerHTML = conteudo;
-  teste.append(table);
- 
+  return data;
 };
 
-const addWorkout = document.querySelector("[data-form-button]");
+function initializeWorktouTable() {
+  const workoutTable = document.querySelector("#table");
+  let tableHeaders = "";
 
-addWorkout.addEventListener("click", createLog);
+  fields.forEach((field) => {
+    const tableHeader = `<th>${field.name}</th>`;
+    tableHeaders += tableHeader;
+  });
+
+  workoutTable.innerHTML = `<tr class="table-headers">${tableHeaders}</tr>`;
+}
+
+function createWorkoutLog(event) {
+
+  event.preventDefault();
+
+  const { time, workout, date } = getWorkoutData();
+
+  if(time == "" || date == ""){
+    return;
+  }
+
+  totalTime += parseInt(time);
+  console.log(totalTime);
+
+  const totalTimeDiv = document.querySelector("#total-time");
+  const contentTotalTime = `<p>${totalTime} hours of exercise </p>`
+  totalTimeDiv.innerHTML = contentTotalTime;
+  
+  const content = `<tr class="item">
+                        <td>${time}h</td>
+                        <td>${workout}</td>
+                        <td>${date}</td>
+
+                    </tr>`;
+
+  const table = document.querySelector("#table");
+
+  const tableRow = table.insertRow();
+  tableRow.innerHTML = content;
+  //tableRow.appendChild(DeleteButton());
+}
+
+const addWorkout = document.querySelector("#form-button");
+addWorkout.addEventListener("click", createWorkoutLog);
+
+window.onload = initializeWorktouTable();
